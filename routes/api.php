76f7a87c03178fa21bf594200fa84f9eb9df76ba
MariamMahrous <mariammahrous80@yahoo.com>
2025-09-05 10:21:37 +0300

    Setup Jwt && Create Middleware for ChackPassword and Language

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+ use App\Http\Controllers\Api\CategoriesController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware(['api','checkPassword'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware'=>['api','checkPassword','changeLanguage'],'namespace'=>'Api'],function(){
+
+    Route::post('getCategories',[CategoriesController::class, 'index']);
+ 
+    
+
+});
